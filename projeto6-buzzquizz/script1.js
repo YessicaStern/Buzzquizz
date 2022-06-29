c524ea7f5415d1e407240b252925be52f5ca10ba
gavuolo <gabs.vuolo@gmail.com>
2022-06-29 15:16:57 -0400

    GET lista de quizzes - tela principal

--- projeto6-buzzquizz/script1.js
+++ projeto6-buzzquizz/script1.js
@@ -1,7 +1,37 @@
-promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-promessa.then(executarPromessa);
+//obter quizzes API, GET: https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes
+//ele envia um objeto com 50 ultimos quizzes;
 
-function executarPromessa(resposta){
-    console.log(resposta);
-    console.log(resposta.data);
+let url = 'https://mock-api.driven.com.br/api/v4/buzzquizz/';
+let quizzes = [];
+let divQuizz = document.querySelector('.all-quizzes');
+
+renderizarQuizzes();
+
+//-------------------------------------------------------------------------------------------
+//catch e then 
+function erroPromessa() {
+    console.log("não funcionou");
+}
+function executarPromessa(resposta) {  
+    quizzes = resposta.data;
+}
+
+
+//-------------------------------------------------------------------------------------------
+function renderizarQuizzes() {
+    let promessa = axios.get(`${url}quizzes`);
+
+    promessa.then(executarPromessa);
+    promessa.catch(erroPromessa);
+    
+
+    for (i = 0 ; i < quizzes.length ; i ++){
+        divQuizz.innerHTML += `
+        <div class="quizz">
+        <img src="${quizzes[i].image}" alt="">
+        <p>${quizzes[i].title}</p>
+        </div>
+        `
+    }
+    
 }
