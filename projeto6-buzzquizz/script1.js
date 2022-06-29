0a5e1c32327d83056d4796eb14db1599476bcd94
gavuolo <gabs.vuolo@gmail.com>
2022-06-29 18:06:30 -0400

    ADD JS

--- projeto6-buzzquizz/script1.js
+++ projeto6-buzzquizz/script1.js
@@ -4,26 +4,24 @@
 let url = 'https://mock-api.driven.com.br/api/v4/buzzquizz/';
 let quizzes = [];
 let divQuizz = document.querySelector('.all-quizzes');
+let meusQuizzes
 
 renderizarQuizzes();
 
+//localStorage.setItem("chave", `${quizzes.id}`)
+//const chave = localStorage.getItem("chave")
+//console.log(chave)
+
 //-------------------------------------------------------------------------------------------
-//catch e then 
+//catch - Erro 
 function erroPromessa() {
     console.log("não funcionou");
 }
-function executarPromessa(resposta) {  
-    quizzes = resposta.data;
-}
-
 
 //-------------------------------------------------------------------------------------------
-function renderizarQuizzes() {
-    let promessa = axios.get(`${url}quizzes`);
-
-    promessa.then(executarPromessa);
-    promessa.catch(erroPromessa);
-    
+//buscando os quizzes da API
+function executarPromessa(resposta) {  
+    quizzes = resposta.data;
 
     for (i = 0 ; i < quizzes.length ; i ++){
         divQuizz.innerHTML += `
@@ -33,5 +31,24 @@ function renderizarQuizzes() {
         </div>
         `
     }
+}
+function renderizarQuizzes() {
+    let promessa = axios.get(`${url}quizzes`);
+
+    promessa.then(executarPromessa);
+    promessa.catch(erroPromessa);    
+}
+
+//--------------------------------------------------------------------------------------------
+// ao clicar no "criar quizz"
+function botaoCriarQuizz (){
+    console.log("clicou")
+    let esconder = document.querySelector('.display-one');
+    let esconder2 = document.querySelector('.display-two');
     
+    esconder.classList.add('esconder');
+    esconder2.classList.add('esconder');
+
+    let aparecer = document.querySelector(".desktop-8");
+    aparecer.classList.remove('esconder')
 }
