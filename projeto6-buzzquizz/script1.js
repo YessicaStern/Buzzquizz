4355e6ee83a4a40aca68a961574a7f20133cd054
gavuolo <gabs.vuolo@gmail.com>
2022-06-30 00:25:38 -0400

    Layout Quizz + funções

--- projeto6-buzzquizz/script1.js
+++ projeto6-buzzquizz/script1.js
@@ -2,13 +2,12 @@
 //ele envia um objeto com 50 ultimos quizzes;
 
 let url = 'https://mock-api.driven.com.br/api/v4/buzzquizz/';
-let quizzes = [];
+let quizzes;
 let divQuizz = document.querySelector('.all-quizzes');
-let meusQuizzes
 
 renderizarQuizzes();
 
-//localStorage.setItem("chave", `${quizzes.id}`)
+//localStorage.setItem("chave", `id`)
 //const chave = localStorage.getItem("chave")
 //console.log(chave)
 
@@ -20,18 +19,24 @@ function erroPromessa() {
 
 //-------------------------------------------------------------------------------------------
 //buscando os quizzes da API
+
 function executarPromessa(resposta) {  
+
     quizzes = resposta.data;
 
+    
     for (i = 0 ; i < quizzes.length ; i ++){
         divQuizz.innerHTML += `
-        <div class="quizz">
-        <img src="${quizzes[i].image}" alt="">
+        <div class="quizz" onclick="abrirQuizz(this)">
+        <img id="${quizzes[i].id}" src="${quizzes[i].image}" alt="">
         <p>${quizzes[i].title}</p>
         </div>
         `
     }
+
+    
 }
+
 function renderizarQuizzes() {
     let promessa = axios.get(`${url}quizzes`);
 
@@ -40,7 +45,7 @@ function renderizarQuizzes() {
 }
 
 //--------------------------------------------------------------------------------------------
-// ao clicar no "criar quizz"
+// ao clicar no "criar quizz" - muda para pagina de criação de quizz
 function botaoCriarQuizz (){
     console.log("clicou")
     let esconder = document.querySelector('.display-one');
@@ -49,6 +54,18 @@ function botaoCriarQuizz (){
     esconder.classList.add('esconder');
     esconder2.classList.add('esconder');
 
-    let aparecer = document.querySelector(".d-9");
+    let aparecer = document.querySelector(".d-8");
     aparecer.classList.remove('esconder')
+}
+
+
+//--------------------------------------------------------------------------------------------
+// CLICAR EM UM QUIZZ
+
+function abrirQuizz(id){
+
+    //o id da imagem que eu clicar esta no id=""
+    console.log(id);
+    console.log("clicou");
+  
 }
