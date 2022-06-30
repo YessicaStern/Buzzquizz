d7e962fcb4fc2809b124ada14c01aacb105f546b
gavuolo <gabs.vuolo@gmail.com>
2022-06-30 14:07:43 -0400

    ADD abrir quizz

--- projeto6-buzzquizz/script1.js
+++ projeto6-buzzquizz/script1.js
@@ -5,6 +5,7 @@
 let url = 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes';
 let quizzes;
 let divQuizz = document.querySelector('.all-quizzes');
+let obj
 
 renderizarQuizzes();
 
@@ -48,7 +49,6 @@ function renderizarQuizzes() {
 //--------------------------------------------------------------------------------------------
 // ao clicar no "criar quizz" - muda para pagina de criação de quizz
 function botaoCriarQuizz (){
-    console.log("clicou")
     let esconder = document.querySelector('.display-one');
     let esconder2 = document.querySelector('.display-two');
     
@@ -61,16 +61,9 @@ function botaoCriarQuizz (){
 
 
 //--------------------------------------------------------------------------------------------
-// CLICAR EM UM QUIZZ
-// chamar um GET PRA https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/ID_DO_QUIZZ
-//ONDE ID DO QUIZZ ESTA NO IDQUIZZ
-let idQuizz
 
 function pegarIdQuizz(click, idQuizz){
-    
-    console.log(click)
-    console.log(idQuizz)
-    //console.log(idQuizz)
+   
     let promessa = axios.get(`${url}/${idQuizz}`);
     promessa.then(abrirQuizz)
     promessa.catch(erroPromessa)
@@ -79,11 +72,7 @@ function pegarIdQuizz(click, idQuizz){
 
 
 function abrirQuizz (resposta){
-    let obj = resposta.data
-
-    
-    console.log(obj.title)
-    console.log(obj.questions[1].title)
+    obj = resposta.data
 
     //esconder pagina anterior
     let esconder = document.querySelector('.display-one');
@@ -97,54 +86,83 @@ function abrirQuizz (resposta){
     aparecer.classList.remove('esconder');
     aparecer2.classList.remove('esconder');
 
+    addTemplateQuizz()
+      
+}
 
-    //adicionar o quizz no template
+function addTemplateQuizz(){
+
     //topo -> adc na div do "quizz-top" 
     let divAdd = document.querySelector('.quizz-top')
-        divAdd.innerHTML += `
-            <div class="top-img">
-                <img src="${obj.image}" alt="">
-                <p>${obj.title}</p>
-            </div>
-            `
-   
+    divAdd.innerHTML += `
+          <div class="top-img">
+              <img src="${obj.image}" alt="">
+              <p>${obj.title}</p>
+          </div>
+          `
+
     //perguntas -> adc na div do "box-img-question"
     //ARRUMAR A COR DA DIV QUESTION
+    let perguntas = obj.questions
 
+
+  
     for(i = 0; i < obj.questions.length ; i++){
+        console.log(obj.questions.length)
+        //onsole.log(perguntas[i].answers[i].image) 
+        //console.log(perguntas[i].answers[i+1].image)
+        //console.log(perguntas[i].answers[i+2].image)
+        //console.log(perguntas[i].answers[i+3].image)
+        //console.log(perguntas[i].answers[i].isCorrectAnswer)
+
         let divQuestion = document.querySelector('.push-question')
-        console.log(divQuestion)
+        
+
+        //adicionar a [imagem - text] corretos; 
+        //aleatorizar as opções
         divQuestion.innerHTML += `
 
         <div class="box-questions">
             <div class="box-img-question">
                 <div class="question">
-                    <p>${obj.questions[i].title}</p>
+                    <p>${perguntas[i].title}</p>
                 </div>
     
     
-                <div class="options">
-                    <img src="${obj.questions[i].answers[i].image}" alt="">
-                    <p>${obj.questions[i].answers[i].text}</p>
+                <div class="options" onclick="respostaVerificar(this, ${perguntas[i].answers[i].isCorrectAnswer})">
+                    <img src="${perguntas[i].answers[i].image}" alt="">
+                    <p>${perguntas[i].answers[i].text}</p>
                 </div>
 
-                <div class="options">
-                    <img src="${obj.questions[i].answers[i].image}" alt="">
-                    <p>${obj.questions[i].answers[i].text}</p>
+                <div class="options" onclick="respostaVerificar(this, ${perguntas[i].answers[i].isCorrectAnswer})>
+                    <img src="${perguntas[i].answers[i].image}" alt="">
+                    <p>${perguntas[i].answers[i].text}</p>
                 </div>
 
-                <div class="options">
-                    <img src="${obj.questions[i].answers[i].image}" alt="">
-                    <p>${obj.questions[i].answers[i].text}</p>
+                <div class="options" onclick="respostaVerificar(this, ${perguntas[i].answers[i].isCorrectAnswer})>
+                    <img src="${perguntas[i].answers[i].image}" alt="">
+                    <p>${perguntas[i].answers[i].text}</p>
                 </div>
             
-                <div class="options">
-                    <img src="${obj.questions[i].answers[i].image}" alt="">
-                    <p>${obj.questions[i].answers[i].text}</p>
+                <div class="options" onclick="respostaVerificar(this, ${perguntas[i].answers[i].isCorrectAnswer})>
+                    <img src="${perguntas[i].answers[i].image}" alt="">
+                    <p>${perguntas[i].answers[i].text}</p>
                 </div>
             </div>
         </div>
             `
-            }
+            }           
 
+}
+
+function respostaVerificar(click, booleanoResposta){
+    console.log(booleanoResposta)
+    console.log(Boolean(booleanoResposta))
+
+    if (Boolean(booleanoResposta)){
+        console.log("voce acertou")
+    }
+    else{
+        console.log("voce errou")
+    }
 }
